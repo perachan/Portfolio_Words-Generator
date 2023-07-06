08a5b6871fc4cc843ddcea9d65573c3b5e77136b
perachan <131749608+perachan@users.noreply.github.com>
2023-07-06 17:04:42 +0700

    Refactoring

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
+const movie = ['2067', 'Jiu Jitsu', 'Love and Monsters', 'The Midnight Sky', 'Skylines', 'Tenet'];
+
+
+// Calculate the number of hidden words based on the movie name length
+function calNumHiddenWord(movieName) {
+    return Math.max(Math.floor(movieName.length * 1.1 - 2), 0);
+}
+
+function ranIndex(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
 // Function to generate a random word from the movie array
-function generateWord() {
-    if (movie.length === 0) {
-      alert('Movie array is empty. Please load movie data first.');
-      return;
-    }
-  
-    const randomIndex = Math.floor(Math.random() * movie.length);
-    const randomWord = movie[randomIndex];
-    document.getElementById('wordPlaceholder').textContent = randomWord;
-  }
-  
-  // Event listener for the "Generate Word" button
-  document.getElementById('generateButton').addEventListener('click', generateWord);
-  
-  // Read the CSV file and store the data in the "movie" array
-  function readCSVFile(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const csv = event.target.result;
-        const lines = csv.split('\n');
-        const movies = [];
-        for (let i = 0; i < lines.length; i++) {
-          const movieData = lines[i].trim();
-          movies.push(movieData);
+function generateMovie(movieArray) {
+    let movieName = ranIndex(movieArray);
+    let tempName = movieName.split(' ');
+    let replaceStr;
+
+    // hide middle part of the name if the name only have 1 word
+    if (!Array.isArray(tempName)) {
+        replaceStr = tempName.slice(1, tempName.length - 1);
+        movieName = movieName.replace(replaceStr, '_ '.repeat(replaceStr.length));
+    } else {
+        // hide X words of the name if the name have more than 1 word
+        const tempNameLength = tempName.length;
+        const numHiddenWord = calNumHiddenWord(tempName);
+        let ranPosition = Math.round(Math.random() * tempNameLength);
+        let ranHistory = [];
+        let replaceStrLenght;
+
+        for (let i = 0; i <= numHiddenWord; i++) {
+            for (i = 0; i <= ranHistory.length; i++) {
+                if (ranPosition === ranHistory[i]) {
+                    ranPosition = Math.round(Math.random() * tempNameLength);
+                }
+            }
+
+            replaceStr = tempName[ranPosition];
+            replaceStrLength = replaceStr.length;
+            movieName = movieName.replace(replaceStr, '_ '.repeat(replaceStr.length));
+            ranHistory.push(ranPosition);
         }
-        resolve(movies);
-      };
-      reader.onerror = (event) => {
-        reject(event.target.error);
-      };
-      reader.readAsText(file);
-    });
-  }
-  
-  // Event listener for loading the CSV file
-  window.addEventListener('DOMContentLoaded', () => {
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.accept = '.csv';
-    fileInput.addEventListener('change', (event) => {
-      const file = event.target.files[0];
-      readCSVFile(file)
-        .then((movies) => {
-          window.movie = movies;
-          console.log('Movie data loaded:', movie);
-        })
-        .catch((error) => {
-          console.error('Error reading CSV file:', error);
-        });
-    });
-    fileInput.click();
-  });
-  
+    }
+
+    return movieName;
+}
+
+
+console.log(generateMovie(movieArray));
+// Event listener for the "Generate" button
+document.getElementById('generateButton').addEventListener('click', generateMovie(movieArray));
